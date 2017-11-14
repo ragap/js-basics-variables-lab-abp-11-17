8e357b0255afebb827f221794477bfd3b46e752a
ragas <sudha.puttagunta@gmail.com>
2017-11-14 10:46:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const companyName = 'Scuber';
 
 let mostProfitableNeighborhood = 'Chelsea';
 
-mostProfitableNeighborhood = 'Upper West Side'
+mostProfitableNeighborhood = 'Upper West Side';
 
 
-
-const companyCeo = 'Susan Smith';
+let companyCeo = 'Susan Smith';
